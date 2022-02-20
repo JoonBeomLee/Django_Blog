67b024572cb3d0d2872ad57bd1bb97c146cf22da
JoonBeomLee <jbl9595@naver.com>
2022-02-21 00:22:40 +0900

    python anywhere  적용

--- MyBlog/MyBlog/settings.py
+++ MyBlog/MyBlog/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = "/home/joonb/Django_Blog/MyBlog/MyBlog/static"
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
