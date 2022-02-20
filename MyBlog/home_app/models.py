45ee5950e2146f106793e6d2cb59743cadc342f8
JoonBeomLee <jbl9595@naver.com>
2022-02-20 23:58:26 +0900

    index page model 등록

--- MyBlog/home_app/models.py
+++ MyBlog/home_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class homeInfo(models.Model):
+    # web site title (사이트 상단에 노출될 이름)
+    siteTitle = models.CharField(max_length=255)
+    # page title (페이지 상단에 노출될 이름)
+    pageTitle = models.CharField(max_length=255)
+    
+class intro(models.Model):
+    title = models.TextField()
+    content = models.TextField()
